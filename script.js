e2ae4ac4a57370206d19df1283418d2b65e1265c
CAI10official <cai10.cmm.kmutt@gmail.com>
2025-10-21 12:55:57 +0700

    update project files all new 6

--- script.js
+++ script.js
@@ -56,4 +56,40 @@ document.addEventListener('DOMContentLoaded', () => {
             overlay.style.display = 'none';
             }
         });
+
+
+
+const isReducedMotion = window.matchMedia('(prefers-reduced-motion:reduce)').matches;
+const isTouch = window.matchMedia('(pointer: coarse').matches;
+const shouldRun = !isReducedMotion && !isTouch;
+
+
+if (shouldRun) {
+    let mouseX =0
+    let mouseY =0
+
+    const pointer = document.querySelector('.pointer');
+    const coordinates = document.querySelector('#coordinates strong');
+
+    window.addEventListener('mousemove' , (e) => {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+
+        // console.log(mouseX, mouseY);
+
+        if (coordinates) {
+            coordinates.innerText = `x: ${mouseX} y: ${mouseY}`;
+        }
+
+    });
+
+    function animate() {
+        pointer.style.setProperty('--mouseX', `${mouseX}px`);
+        pointer.style.setProperty('--mouseY', `${mouseY}px`);
+        requestAnimationFrame(animate);
+
+    }
+
+    animate();
+}
   
